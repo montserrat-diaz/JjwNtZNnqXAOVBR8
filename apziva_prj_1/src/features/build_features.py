88f7cf15473cd9af6d433d66c9857864715c54f1
Montserrat Diaz Marroquin <montserrat.dmarroquin@gmail.com>
2023-07-31 17:17:13 -0600

    Update build_features.py

--- apziva_prj_1/src/features/build_features.py
+++ apziva_prj_1/src/features/build_features.py
@@ -1,7 +1,18 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
-def extract_features(data):
-    """Extract the features from the data."""
+def preprocess_data(data):
     X = data.drop(["Y"], axis=1)
     y = data["Y"]
-    return X, y
+
+    # standardize the dataset
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    # split into train and test set
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_scaled, y, stratify=y, test_size=0.25, random_state=42
+    )
+
+    return X_train, X_test, y_train, y_test
