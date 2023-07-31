fb326e6616f08aac2fdca54a28037f55af0b6fd8
Montserrat Diaz Marroquin <montserrat.dmarroquin@gmail.com>
2023-07-31 17:24:11 -0600

    Update train_model.py

--- apziva_prj_1/src/models/train_model.py
+++ apziva_prj_1/src/models/train_model.py
@@ -1,17 +1,5 @@
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 
-np.random.seed(42)
-
-def preprocess_and_split_data(X, y, test_size=0.25, random_state=42):
-    # standardize the dataset
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
-
-    # split into train and test set
-    X_train, X_test, y_train, y_test = train_test_split(
-        X_scaled, y, stratify=y, test_size=test_size, random_state=random_state
-    )
-
-    return X_train, X_test, y_train, y_test
+def train_model(classifier, X_train, y_train):
+    """Train the model using the training sets."""
+    classifier.fit(X_train, y_train)
