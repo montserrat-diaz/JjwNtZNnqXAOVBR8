45695eabff5381fab0d2a3b40e5d2fd52449988d
Montserrat Diaz Marroquin <montserrat.dmarroquin@gmail.com>
2023-07-31 17:13:30 -0600

    Update predict_model.py

--- apziva_prj_1/src/models/predict_model.py
+++ apziva_prj_1/src/models/predict_model.py
@@ -1,15 +1,5 @@
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 
-def preprocess_and_split_data(X, y, test_size=0.25, random_state=42):
-    """Preprocess the data and split it into train and test sets."""
-    # standardize the dataset
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
-
-    # split into train and test set
-    X_train, X_test, y_train, y_test = train_test_split(
-        X_scaled, y, stratify=y, test_size=test_size, random_state=random_state
-    )
-
-    return X_train, X_test, y_train, y_test
+def predict_model(classifier, X_test):
+    y_pred = classifier.predict(X_test)
+    return y_pred
