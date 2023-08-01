1685072ffad5eb3a2d72eb594407d0744656798b
Montserrat Diaz Marroquin <montserrat.dmarroquin@gmail.com>
2023-07-31 22:11:16 -0600

    Update visualize.py

---
+++ apziva_prj_1/src/visualization/visualize.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plot_actual_vs_predicted(y_true, y_pred):
+    """Plot the difference between actual and predicted values using Seaborn."""
+    plt.figure(figsize=(5, 5))
+
+    ax = sns.distplot(y_true, hist=False, color="orange", label="Actual Value")
+    sns.distplot(y_pred, hist=False, color="green", label="Predicted Values", ax=ax)
+
+    plt.title('Actual vs Predicted Values')
+    plt.legend()
+    plt.show()
+    plt.close()
+
+plot_actual_vs_predicted(data["Y"], y_pred)
