bd460fea0ebd77e2bece8d4065b6efba2eb52e4c
Montserrat Diaz Marroquin <montserrat.dmarroquin@gmail.com>
2023-07-31 23:02:41 -0600

    Update train_model.py

--- apziva_prj_1/src/models/train_model.py
+++ apziva_prj_1/src/models/train_model.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from build_features import X_train, y_train
 
 def train_model(classifier, X_train, y_train):
     classifier.fit(X_train, y_train)
