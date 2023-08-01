45cda80201f4571416db77cbaf13367ebc1e1c00
Montserrat Diaz Marroquin <montserrat.dmarroquin@gmail.com>
2023-07-31 19:13:20 -0600

    Update predict_model.py

--- apziva_prj_1/src/models/predict_model.py
+++ apziva_prj_1/src/models/predict_model.py
@@ -1,6 +1,12 @@
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 def predict_model(classifier, X_test):
     y_pred = classifier.predict(X_test)
     return y_pred
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Precision: ", precision_score(y_test, y_pred))
+print("Recall: ", recall_score(y_test, y_pred))
+
